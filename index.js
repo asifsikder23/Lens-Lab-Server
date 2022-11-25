57736611852652b2af4799e116648d76c53634da
Md. Asif Ullah Sikder <asifsikder23@gmail.com>
2022-11-25 09:55:11 +0600

    lens-lab-server

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const cors = require('cors');
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+app.use(cors());0
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0iyuemt.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
+
+async function run(){
+    try{
+        await client.connect();
+        const categoryCollection = client.db('lens-lab').collection('Categories');
+
+        app.get('/categories', async(req,res)=>{
+            const query = {};
+            const result = await categoryCollection.find(query).toArray();
+            res.send(result);
+        })
+
+    }
+    finally{
+
+    }
+}
+run().catch(console.log);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
