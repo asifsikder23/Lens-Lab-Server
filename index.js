ba632543f91da08e39170b9da50a5ef4de2dcf79
Md. Asif Ullah Sikder <asifsikder23@gmail.com>
2022-11-27 12:40:24 +0600

    make admin

--- index.js
+++ index.js
@@ -1,69 +1,80 @@
-const express = require('express')
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-app.use(cors());0
+app.use(cors());
+0;
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0iyuemt.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run(){
-    try{
-        await client.connect();
-        const usersCollection = client.db('lens-lab').collection('users');
-        const categoryCollection = client.db('lens-lab').collection('Categories');
-        const dslrCollection = client.db('lens-lab').collection('dslrCamera');
+async function run() {
+  try {
+    await client.connect();
+    const usersCollection = client.db("lens-lab").collection("users");
+    const categoryCollection = client.db("lens-lab").collection("Categories");
+    const dslrCollection = client.db("lens-lab").collection("dslrCamera");
 
-
-        app.post('/users', async(req, res)=>{
-          const user = req.body;
-          console.log(user);
-          const result = await usersCollection.insertOne(user);
-          res.send(result);
-      })
-      app.get('/users', async(req,res)=>{
-        const query = {};
-        const result = await usersCollection.find(query).toArray();
-        res.send(result);
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const result = await usersCollection.find(query).toArray();
+      res.send(result);
+    });
+    // admin route
+    app.put('/users/admin/:id', async(req, res)=>{
+      const id = req.params.id;
+      const filter = {_id: ObjectId(id)}
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          role: 'Admin'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc, options);
+      res.send(result);
     })
 
-        
-
-        app.get('/categories', async(req,res)=>{
-            const query = {};
-            const result = await categoryCollection.find(query).toArray();
-            res.send(result);
-        })
-
-        app.get('/categories/:id', async(req,res)=>{
-          const id = req.params.id;
-          console.log(id);
-            const query = {categoriesId:id};
-            const result = await dslrCollection.find(query).toArray();
-            res.send(result);
-        })
-      app.post('/categories', async (req, res) => {
-        const query = req.body;
-        const result = await dslrCollection.insertOne(query);
-        res.send(result);
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const result = await categoryCollection.find(query).toArray();
+      res.send(result);
     });
 
-    }
-    finally{
-
-    }
+    app.get("/categories/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { categoriesId: id };
+      const result = await dslrCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.post("/categories", async (req, res) => {
+      const query = req.body;
+      const result = await dslrCollection.insertOne(query);
+      res.send(result);
+    });
+  } finally {
+  }
 }
 run().catch(console.log);
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
